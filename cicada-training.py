d96285255aa1820b3e23a0abd2908444c7e5c240
ligerlac <lino.oscar.gerlach@cern.ch>
2024-07-30 16:22:37 +0200

    remove plotting from training script

--- cicada-training.py
+++ cicada-training.py
@@ -6,7 +6,6 @@ os.environ["TF_ENABLE_ONEDNN_OPTS"] = "0"
 import argparse
 import numpy as np
 import numpy.typing as npt
-import pandas as pd
 import yaml
 
 from pathlib import Path
@@ -16,8 +15,7 @@ from tensorflow.keras.models import load_model
 from tensorflow.keras.callbacks import ModelCheckpoint, CSVLogger
 from tensorflow.keras.optimizers import Adam
 
-from utils import IsValidFile
-from drawing import Draw
+from utils import IsValidFile, CreateFolder
 from generator import RegionETGenerator
 from models import TeacherAutoencoder, CicadaV1, CicadaV2
 
@@ -65,10 +63,9 @@ def train_model(
     )
 
 
-def run_training(
-    config: dict, eval_only: bool, epochs: int = 100, verbose: bool = False
-) -> None:
-    draw = Draw()
+def main(args) -> None:
+
+    config = yaml.safe_load(open(args.config))
 
     datasets = [i["path"] for i in config["background"] if i["use"]]
     datasets = [path for paths in datasets for path in paths]
@@ -84,190 +81,81 @@ def run_training(
     X_train_student = np.concatenate([X_train, outlier_train])
     X_val_student = np.concatenate([X_val, outlier_train])
 
-    if not eval_only:
-        teacher = TeacherAutoencoder((18, 14, 1)).get_model()
-        teacher.compile(optimizer=Adam(learning_rate=0.001), loss="mse")
-        t_mc = ModelCheckpoint(f"models/{teacher.name}", save_best_only=True)
-        t_log = CSVLogger(f"models/{teacher.name}/training.log", append=True)
+    teacher = TeacherAutoencoder((18, 14, 1)).get_model()
+    teacher.compile(optimizer=Adam(learning_rate=0.001), loss="mse")
+    t_mc = ModelCheckpoint(f"{args.output}/{teacher.name}", save_best_only=True)
+    t_log = CSVLogger(f"{args.output}/{teacher.name}/training.log", append=True)
 
-        cicada_v1 = CicadaV1((252,)).get_model()
-        cicada_v1.compile(optimizer=Adam(learning_rate=0.001), loss="mae")
-        cv1_mc = ModelCheckpoint(f"models/{cicada_v1.name}", save_best_only=True)
-        cv1_log = CSVLogger(f"models/{cicada_v1.name}/training.log", append=True)
+    cicada_v1 = CicadaV1((252,)).get_model()
+    cicada_v1.compile(optimizer=Adam(learning_rate=0.001), loss="mae")
+    cv1_mc = ModelCheckpoint(f"{args.output}/{cicada_v1.name}", save_best_only=True)
+    cv1_log = CSVLogger(f"{args.output}/{cicada_v1.name}/training.log", append=True)
 
-        cicada_v2 = CicadaV2((252,)).get_model()
-        cicada_v2.compile(optimizer=Adam(learning_rate=0.001), loss="mae")
-        cv2_mc = ModelCheckpoint(f"models/{cicada_v2.name}", save_best_only=True)
-        cv2_log = CSVLogger(f"models/{cicada_v2.name}/training.log", append=True)
+    cicada_v2 = CicadaV2((252,)).get_model()
+    cicada_v2.compile(optimizer=Adam(learning_rate=0.001), loss="mae")
+    cv2_mc = ModelCheckpoint(f"{args.output}/{cicada_v2.name}", save_best_only=True)
+    cv2_log = CSVLogger(f"{args.output}/{cicada_v2.name}/training.log", append=True)
 
-        for epoch in range(epochs):
-            train_model(
-                teacher,
-                gen_train,
-                gen_val,
-                epoch=epoch,
-                callbacks=[t_mc, t_log],
-                verbose=verbose,
-            )
-
-            tmp_teacher = load_model("models/teacher")
-            s_gen_train = get_student_targets(tmp_teacher, gen, X_train_student)
-            s_gen_val = get_student_targets(tmp_teacher, gen, X_val_student)
-
-            train_model(
-                cicada_v1,
-                s_gen_train,
-                s_gen_val,
-                epoch=10 * epoch,
-                steps=10,
-                callbacks=[cv1_mc, cv1_log],
-                verbose=verbose,
-            )
-            train_model(
-                cicada_v2,
-                s_gen_train,
-                s_gen_val,
-                epoch=10 * epoch,
-                steps=10,
-                callbacks=[cv2_mc, cv2_log],
-                verbose=verbose,
-            )
-
-        for model in [teacher, cicada_v1, cicada_v2]:
-            log = pd.read_csv(f"models/{model.name}/training.log")
-            draw.plot_loss_history(
-                log["loss"], log["val_loss"], f"{model.name}-training-history"
-            )
-
-    teacher = load_model("models/teacher")
-    cicada_v1 = load_model("models/cicada-v1")
-    cicada_v2 = load_model("models/cicada-v2")
-
-    # Comparison between original and reconstructed inputs
-    X_example = X_test[:1]
-    y_example = teacher.predict(X_example, verbose=verbose)
-    draw.plot_reconstruction_results(
-        X_example,
-        y_example,
-        loss=loss(X_example, y_example)[0],
-        name="comparison-background",
-    )
-    X_example = X_signal["SUSYGGBBH"][:1]
-    y_example = teacher.predict(X_example, verbose=verbose)
-    draw.plot_reconstruction_results(
-        X_example,
-        y_example,
-        loss=loss(X_example, y_example)[0],
-        name="comparison-signal",
-    )
-
-    # Evaluation
-    y_pred_background_teacher = teacher.predict(X_test, batch_size=512, verbose=verbose)
-    y_loss_background_teacher = loss(X_test, y_pred_background_teacher)
-    y_loss_background_cicada_v1 = cicada_v1.predict(
-        X_test.reshape(-1, 252, 1), batch_size=512, verbose=verbose
-    )
-    y_loss_background_cicada_v2 = cicada_v2.predict(
-        X_test.reshape(-1, 252, 1), batch_size=512, verbose=verbose
-    )
-
-    results_teacher, results_cicada_v1, results_cicada_v2 = dict(), dict(), dict()
-    results_teacher["2023 Zero Bias (Test)"] = y_loss_background_teacher
-    results_cicada_v1["2023 Zero Bias (Test)"] = y_loss_background_cicada_v1
-    results_cicada_v2["2023 Zero Bias (Test)"] = y_loss_background_cicada_v2
-
-    y_true, y_pred_teacher, y_pred_cicada_v1, y_pred_cicada_v2 = [], [], [], []
-    inputs = []
-    for name, data in X_signal.items():
-        inputs.append(np.concatenate((data, X_test)))
-
-        y_loss_teacher = loss(
-            data, teacher.predict(data, batch_size=512, verbose=verbose)
-        )
-        y_loss_cicada_v1 = cicada_v1.predict(
-            data.reshape(-1, 252, 1), batch_size=512, verbose=verbose
-        )
-        y_loss_cicada_v2 = cicada_v2.predict(
-            data.reshape(-1, 252, 1), batch_size=512, verbose=verbose
-        )
-        results_teacher[name] = y_loss_teacher
-        results_cicada_v1[name] = y_loss_cicada_v1
-        results_cicada_v2[name] = y_loss_cicada_v2
-
-        y_true.append(
-            np.concatenate((np.ones(data.shape[0]), np.zeros(X_test.shape[0])))
-        )
-        y_pred_teacher.append(
-            np.concatenate((y_loss_teacher, y_loss_background_teacher))
-        )
-        y_pred_cicada_v1.append(
-            np.concatenate((y_loss_cicada_v1, y_loss_background_cicada_v1))
-        )
-        y_pred_cicada_v2.append(
-            np.concatenate((y_loss_cicada_v2, y_loss_background_cicada_v2))
+    for epoch in range(args.epochs):
+        train_model(
+            teacher,
+            gen_train,
+            gen_val,
+            epoch=epoch,
+            callbacks=[t_mc, t_log],
+            verbose=args.verbose,
         )
 
-    draw.plot_anomaly_score_distribution(
-        list(results_teacher.values()),
-        [*results_teacher],
-        "anomaly-score-teacher",
-    )
-    draw.plot_anomaly_score_distribution(
-        list(results_cicada_v1.values()),
-        [*results_cicada_v1],
-        "anomaly-score-cicada-v1",
-    )
-    draw.plot_anomaly_score_distribution(
-        list(results_cicada_v2.values()),
-        [*results_cicada_v2],
-        "anomaly-score-cicada-v2",
-    )
+        tmp_teacher = load_model(f"{args.output}/teacher")
+        s_gen_train = get_student_targets(tmp_teacher, gen, X_train_student)
+        s_gen_val = get_student_targets(tmp_teacher, gen, X_val_student)
 
-    # ROC Curves with Cross-Validation
-    draw.plot_roc_curve(y_true, y_pred_teacher, [*X_signal], inputs, "roc-teacher")
-    draw.plot_roc_curve(y_true, y_pred_cicada_v1, [*X_signal], inputs, "roc-cicada-v1")
-    draw.plot_roc_curve(y_true, y_pred_cicada_v2, [*X_signal], inputs, "roc-cicada-v2")
+        train_model(
+            cicada_v1,
+            s_gen_train,
+            s_gen_val,
+            epoch=10 * epoch,
+            steps=10,
+            callbacks=[cv1_mc, cv1_log],
+            verbose=args.verbose,
+        )
+        train_model(
+            cicada_v2,
+            s_gen_train,
+            s_gen_val,
+            epoch=10 * epoch,
+            steps=10,
+            callbacks=[cv2_mc, cv2_log],
+            verbose=args.verbose,
+        )
 
 
-def parse_arguments():
+if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="""CICADA training scripts""")
     parser.add_argument(
-        "--config",
-        "-c",
+        "--config", "-c",
         action=IsValidFile,
         type=Path,
         default="misc/config.yml",
         help="Path to config file",
     )
     parser.add_argument(
-        "--evaluate-only",
-        action="store_true",
-        help="Skip training",
-        default=False,
+        "--output", "-o",
+        action=CreateFolder,
+        type=Path,
+        default="models/",
+        help="Path to directory where models will be stored",
     )
     parser.add_argument(
-        "-e",
-        "--epochs",
+        "-e", "--epochs",
         type=int,
         help="Number of training epochs",
         default=100,
     )
     parser.add_argument(
-        "-v",
-        "--verbose",
+        "-v", "--verbose",
         action="store_true",
         help="Output verbosity",
         default=False,
     )
-    args = parser.parse_args()
-    config = yaml.safe_load(open(args.config))
-    return args, config
-
-
-def main(args_in=None) -> None:
-    args, config = parse_arguments()
-    run_training(config, args.evaluate_only, epochs=args.epochs, verbose=args.verbose)
-
-
-if __name__ == "__main__":
-    main()
+    main(parser.parse_args())
