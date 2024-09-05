ae3c8b477411306b2b7a551763952a3f4bef24a3
ligerlac <lino.oscar.gerlach@cern.ch>
2024-09-06 00:10:33 +0200

    revised teacher w/ learnable upsampling

--- models.py
+++ models.py
@@ -9,6 +9,7 @@ from tensorflow.keras.layers import (
     Input,
     Reshape,
     UpSampling2D,
+    Conv2DTranspose
 )
 from qkeras import QActivation, QConv2D, QDense, QDenseBatchnorm, quantized_bits
 
@@ -44,6 +45,39 @@ class TeacherAutoencoder:
             name="teacher_outputs",
         )(x)
         return Model(inputs, outputs, name="teacher")
+
+
+class TeacherAutoencoderRevised:
+    def __init__(self, input_shape: tuple):
+        self.input_shape = input_shape
+
+    def get_model(self):
+        inputs = Input(shape=self.input_shape, name="teacher_inputs_")
+        x = Reshape((18, 14, 1), name="teacher_reshape")(inputs)
+        x = Conv2D(20, (3, 3), strides=1, padding="same", name="teacher_conv2d_1")(x)
+        x = Activation("relu", name="teacher_relu_1")(x)
+        x = AveragePooling2D((2, 2), name="teacher_pool_1")(x)
+        x = Conv2D(30, (3, 3), strides=1, padding="same", name="teacher_conv2d_2")(x)
+        x = Activation("relu", name="teacher_relu_2")(x)
+        x = Flatten(name="teacher_flatten")(x)
+        x = Dense(80, activation="relu", name="teacher_latent")(x)
+        x = Dense(9 * 7 * 30, name="teacher_dense")(x)
+        x = Reshape((9, 7, 30), name="teacher_reshape2")(x)
+        x = Activation("relu", name="teacher_relu_3")(x)
+        x = Conv2D(30, (3, 3), strides=1, padding="same", name="teacher_conv2d_3")(x)
+        x = Activation("relu", name="teacher_relu_4")(x)
+        x = Conv2DTranspose(30, (3, 3), strides=2, padding="same", name="teacher_conv_transpose")(x)
+        x = Conv2D(20, (3, 3), strides=1, padding="same", name="teacher_conv2d_4")(x)
+        x = Activation("relu", name="teacher_relu_5")(x)
+        outputs = Conv2D(
+            1,
+            (3, 3),
+            activation="relu",
+            strides=1,
+            padding="same",
+            name="teacher_outputs",
+        )(x)
+        return Model(inputs, outputs, name="teacher-transpose")
 
 
 class CicadaV1:
